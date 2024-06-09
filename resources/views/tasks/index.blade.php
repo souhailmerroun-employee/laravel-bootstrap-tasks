678b160d67f05ad84d85a6f2981f4d26d3dd164b
Souhail Merroun <souhail.merroun1996@gmail.com>
2024-06-09 18:53:49 +0200

    code refactor

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,8 +1,7 @@
-@extends('layout')
+@extends('layouts.app')
 
 @section('content')
     <h1>Task List</h1>
 
     @include('tasks.index_table', ['tasks' => $tasks])
-
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
-Route::redirect('/', '/tasks');
-
-Route::resource('tasks', TaskController::class);
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::redirect('/', '/tasks');
+
+Route::resource('tasks', TaskController::class);
