6a8eaf7a0be008aa4e8650879ce52095ab49d649
Souhail Merroun <souhail.merroun1996@gmail.com>
2024-06-13 09:42:48 +0200

    add js confirmation

--- resources/views/tasks/delete_form.blade.php
+++ resources/views/tasks/delete_form.blade.php
@@ -1,6 +1,6 @@
 <form action="{{ route('tasks.destroy', $task->id) }}" method="POST" style="display:inline;">
     @csrf
     @method('DELETE')
-    <button type="submit" class="btn btn-link text-danger">Delete</button>
+    <button type="submit" class="btn btn-link text-danger" onclick="return confirm('Are you sure you want to do this action?');">Delete</button>
 </form>
 @include('debug.form_action', ['routeName' => 'tasks.destroy'])
